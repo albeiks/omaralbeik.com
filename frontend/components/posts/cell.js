f464cb656324e7abfe20305e24ed23504e8af955
Omar Albeik <omar@backbase.com>
2020-05-13 01:07:13 +0200

    fix: fix post cell link

--- frontend/components/posts/cell.js
+++ frontend/components/posts/cell.js
@@ -21,8 +21,8 @@ class PostCell extends Component {
     const readTime = `${post.read_time} ${strings.read}`;
     return (
       <StyledCol md={type === "latest" ? 12 : 6}>
-        <Link href={url.templateUrl} title={url.name} as={url.url}>
-          <a className={`inner ${type}`} href={url.templateUrl} title={url.name}>
+        <Link href={url.templateUrl} as={url.url} title={url.name}>
+          <a className={`inner ${type}`} href={url.url} title={url.name}>
             <Moment className="date" format="D/M/YYYY">{post.date_published}</Moment>
             <h2 className={type}>{post.title}</h2>
             <p className="summary">{post.summary}</p>
